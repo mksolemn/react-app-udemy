aabf861f5099f57c3cdc49e4d64cd996d34c6a23
Min <mksolemn@gmail.com>
2020-09-25 15:41:39 +0200

    Updates

---
+++ precache-manifest.5b0233acae5cb62e9e4733189a6a9fd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f626ad445fa179a28dc0fd6b017b5321",
+    "url": "/react-app-udemy/index.html"
+  },
+  {
+    "revision": "bb891faf56c503aa6eaf",
+    "url": "/react-app-udemy/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "af9e92478d9178045b93",
+    "url": "/react-app-udemy/static/css/main.8476f8ab.chunk.css"
+  },
+  {
+    "revision": "bb891faf56c503aa6eaf",
+    "url": "/react-app-udemy/static/js/2.a30e3dab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-udemy/static/js/2.a30e3dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af9e92478d9178045b93",
+    "url": "/react-app-udemy/static/js/main.acc86362.chunk.js"
+  },
+  {
+    "revision": "8d55dc55eaea55863d8b",
+    "url": "/react-app-udemy/static/js/runtime-main.1c9e0041.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-app-udemy/static/media/SEGA.12a3db8b.woff"
+  }
+]);
